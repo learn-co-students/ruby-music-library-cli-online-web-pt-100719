252a15a3ce0a6b13f65f3b79a4282f2c74c6e84f
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-15 04:44:02 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -20,5 +20,11 @@ class MusicLibraryController
     end
   end
   
+  def list_songs
+    Song.all.sort{|a,b| a.name <=> b.name}.each_with_index do |song,index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
+  
   
 end
