3abccb6ffd84376a57064fd6aa92ce12c29053b2
Jeff Eckert <bromodo@gmail.com>
2019-11-17 19:27:18 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 require 'bundler'
+
+require_relative '../lib/concerns/findable'
+
 Bundler.require
+
+# require_relative "../lib/song.rb"
+# require_relative "../lib/artist.rb"
+# require_relative "../lib/genre.rb"
 
 module Concerns
 end
